586f1e5958dabb06645aaf0baf80ebbd78c55654
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-08 10:49:45 -0300

    Interfaces da Camada Repository

--- src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -2,5 +2,7 @@ package me.dio.credit.application.system.repository
 
 import me.dio.credit.application.system.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface CreditRepository : JpaRepository<Credit, Long>
+@Repository
+interface CreditRepository: JpaRepository<Credit, Long>
--- src/main/kotlin/me/dio/credit/application/system/repository/CustomerRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/repository/CustomerRepository.kt
@@ -2,5 +2,7 @@ package me.dio.credit.application.system.repository
 
 import me.dio.credit.application.system.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface CustomerRepository : JpaRepository<Customer, Long>
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long>
