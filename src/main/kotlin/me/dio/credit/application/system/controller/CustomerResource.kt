80feb4bf840db7e0989066bcecf9e1c50945e133
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-09 18:38:18 -0300

    Endpoints CustomerResource - (findById, delete)

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -1,7 +1,12 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerView
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,6 +23,16 @@ class CustomerResource(
     val savedCustomer = this.customerService.save(customerDto.toEntity())
     return "Customer ${savedCustomer.email} saved!"
   }
+
+  @GetMapping("/{id}")
+  fun findById(@PathVariable id: Long) : CustomerView {
+    val customer : Customer = this.customerService.findById(id)
+    return CustomerView(customer)
+  }
+
+  @DeleteMapping("/{id}")
+  fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
 
 
 
