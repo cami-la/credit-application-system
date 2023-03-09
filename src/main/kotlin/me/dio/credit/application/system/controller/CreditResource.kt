7480171ec2727092715b484256e3d82738e804cc
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-09 20:52:55 -0300

    Endpoints CreditResource - (findByCreditCode e findAllCustomerCredits)

--- src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -1,12 +1,19 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CreditDto
+import me.dio.credit.application.system.dto.CreditView
+import me.dio.credit.application.system.dto.CreditViewList
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -20,9 +27,18 @@ class CreditResource(
     return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
   }
 
+  @GetMapping
+  fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long) : List<CreditViewList> {
+    return this.creditService.findAllByCustomer(customerId).stream()
+      .map { credit: Credit -> CreditViewList(credit) }
+      .collect(Collectors.toList())
+  }
 
-
-
-
+  @GetMapping
+  fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long,
+                       @PathVariable creditCode: UUID) : CreditView {
+    val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+    return CreditView(credit)
+  }
 
 }
---
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditView.kt
@@ -0,0 +1,24 @@
+package me.dio.credit.application.system.dto
+
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.enummeration.Status
+import java.math.BigDecimal
+import java.util.*
+
+data class CreditView(
+  val creditCode: UUID,
+  val creditValue: BigDecimal,
+  val numberOfInstallment: Int,
+  val status: Status,
+  val emailCustomer: String?,
+  val incomeCustomer: BigDecimal?
+) {
+  constructor(credit: Credit) : this(
+    creditCode = credit.creditCode,
+    creditValue = credit.creditValue,
+    numberOfInstallment = credit.numberOfInstallments,
+    status = credit.status,
+    emailCustomer = credit.customer?.email,
+    incomeCustomer = credit.customer?.income
+  )
+}
