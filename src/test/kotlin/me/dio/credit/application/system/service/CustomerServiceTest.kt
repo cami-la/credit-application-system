cc7cb9367ddfba0781c645d0f7e166bcb2627f0a
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-16 08:14:34 -0300

    Configurando o application-test.properties

---
+++ src/test/kotlin/me/dio/credit/application/system/service/CustomerServiceTest.kt
@@ -0,0 +1,11 @@
+package me.dio.credit.application.system.service
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+
+}
