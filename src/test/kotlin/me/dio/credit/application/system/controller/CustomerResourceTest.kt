e50be20e90ee5f291fb68e22676ec566aca47c8e
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-16 13:53:32 -0300

    Teste de Integração: CustomerResource - save - parte 1

--- src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -1,15 +1,23 @@
 package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.credit.application.system.dto.request.CustomerDto
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,4 +35,43 @@ class CustomerResourceTest {
   @BeforeEach fun setup() = customerRepository.deleteAll()
   @AfterEach fun tearDown() = customerRepository.deleteAll()
 
+  @Test
+  fun `should create a customer and return 201 status`() {
+    //given
+    val customerDto: CustomerDto = builderCustomerDto()
+    val valueAsString: String = objectMapper.writeValueAsString(customerDto)
+    //when
+    //then
+    mockMvc.perform(MockMvcRequestBuilders.post(URL)
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(valueAsString))
+      .andExpect(MockMvcResultMatchers.status().isCreated)
+      .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Cami"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Cavalcante"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("28475934625"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("camila@email.com"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("000000"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua da Cami, 123"))
+      .andDo(MockMvcResultHandlers.print())
+  }
+
+  private fun builderCustomerDto(
+    firstName: String = "Cami",
+    lastName: String = "Cavalcante",
+    cpf: String = "28475934625",
+    email: String = "camila@email.com",
+    income: BigDecimal = BigDecimal.valueOf(1000.0),
+    password: String = "1234",
+    zipCode: String = "000000",
+    street: String = "Rua da Cami, 123",
+  ) = CustomerDto(
+    firstName = firstName,
+    lastName = lastName,
+    cpf = cpf,
+    email = email,
+    income = income,
+    password = password,
+    zipCode = zipCode,
+    street = street
+  )
 }
