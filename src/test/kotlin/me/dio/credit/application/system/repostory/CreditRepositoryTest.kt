c1330a0cf7ab2c390f431353b65084a86bd4e08c
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-03-16 12:34:52 -0300

    Teste de Integração: CreditRepository - findAllByCustomerId

--- src/test/kotlin/me/dio/credit/application/system/repostory/CreditRepositoryTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/repostory/CreditRepositoryTest.kt
@@ -52,8 +52,17 @@ class CreditRepositoryTest {
     Assertions.assertThat(fakeCredit2).isSameAs(credit2)
   }
 
-
-
+  @Test
+  fun `should find all credits by customer id`() {
+    //given
+    val customerId: Long =  1L
+    //when
+    val creditList: List<Credit> = creditRepository.findAllByCustomerId(customerId)
+    //then
+    Assertions.assertThat(creditList).isNotEmpty
+    Assertions.assertThat(creditList.size).isEqualTo(2)
+    Assertions.assertThat(creditList).contains(credit1, credit2)
+  }
 
   private fun buildCredit(
     creditValue: BigDecimal = BigDecimal.valueOf(500.0),
